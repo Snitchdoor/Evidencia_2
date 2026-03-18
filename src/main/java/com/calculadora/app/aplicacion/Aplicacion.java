5fb6f1d3d6770df12e72c1843be6c394b7f42980
Snitchdoor <esparzacarolina526@yahoo.com>
2026-03-18 16:47:04 -0500

    Mi primer commit - Ingreso al Sistema

---
+++ src/main/java/com/calculadora/app/aplicacion/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.calculadora.app.aplicacion;
+
+/**
+ *
+ * @author espar
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
